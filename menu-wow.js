e20482b64d7a95ac0bcab15a427f25b1fb9dd7a4
German <gedaniel1@hotmail.com>
2023-07-03 23:05:19 -0300

    terminado

--- menu-wow.js
+++ menu-wow.js
@@ -1,8 +1,7 @@
 const menu = document.getElementById("menu");
 
-Array.from(document.getElementsByClassName("menu-item"))
-    .forEach((item, index) => {
-        item.onmouseover = () => {
-            menu.dataset.activeIndex = index;
-        }
-    });
+Array.from(document.getElementsByClassName("menu-item")).forEach((item, index) => {
+    item.onmouseover = () => {
+        menu.dataset.activeIndex = index;
+    }
+});
